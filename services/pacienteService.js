c1b3d08f64519e66e5a03e446a9d3b4c05cf42d1
Kuki Amarilla <sc.amarilla@gmail.com>
2021-11-10 14:15:54 -0300

    Filtro Select

--- services/pacienteService.js
+++ services/pacienteService.js
@@ -3,5 +3,8 @@ import api from "./api";
 export default {
   users() {
     return api.get(`/stock-nutrinatalia/persona?ejemplo=%7B%22soloUsuariosDelSistema%22%3Atrue%7D`).then(response => response.data);
+  },
+  clientes() {
+    return api.get(`/stock-nutrinatalia/persona`).then(response => response.data.lista);
   }
 }
