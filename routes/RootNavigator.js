48745f56419a2503e3f9b1a450283d0c53af0a91
Nerea Ortiz <nmoortiz@gmail.com>
2021-11-10 13:14:39 -0300

    Root actualizado

--- routes/RootNavigator.js
+++ routes/RootNavigator.js
@@ -2,15 +2,8 @@ import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import Login from "../views/Login";
-<<<<<<< HEAD
-// import Reservas from "../views/Reservas";
-import Home from "../views/Home";
-import Paciente from "../views/Paciente";
-import Reservas from "../views/Reservas";
-=======
-import Reservas from "../views/Reservas";
 import Pacientes from "../views/Pacientes";
->>>>>>> 96bd0bdc465cc0227a66c3702c3c8f48ce3801b1
+import Reservas from "../views/Reservas";
 import useAuth from "../hooks/useAuth";
 import ReservasAlta from "../views/ReservasAlta";
 
