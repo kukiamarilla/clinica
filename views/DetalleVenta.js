303185a313a09b024f1b16072c97580fc50cde3b
Kuki Amarilla <sc.amarilla@gmail.com>
2021-12-01 07:11:26 -0300

    Agregado cabecera a detalle de factura

--- views/DetalleVenta.js
+++ views/DetalleVenta.js
@@ -1,8 +1,8 @@
 import { StatusBar } from "expo-status-bar";
 import React from "react";
-import { StyleSheet, View } from "react-native";
+import { StyleSheet, Text, View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
-import { Colors } from "../styles/constants";
+import { Colors, Fonts } from "../styles/constants";
 import Header from "../components/ui/Header";
 import { ScrollView } from "react-native-gesture-handler";
 import Detalle from "../components/Detalle";
@@ -21,6 +21,14 @@ const styles = StyleSheet.create({
   }
 });
 
+const formatDate = (date) => {
+  const d = new Date(date);
+  const month = `0${d.getMonth() + 1}`.slice(-2);
+  const day = `0${d.getDate()}`.slice(-2);
+  const year = d.getFullYear();
+  return `${day}/${month}/${year}`;
+}
+
 export default function DetalleVenta({ route }) {
   const { venta } = route.params
 
@@ -31,6 +39,12 @@ export default function DetalleVenta({ route }) {
         <Header title="Detalle de Venta" showMenu >
         </Header>
         <ScrollView style={styles.body}>
+          <Text style={{marginTop: 16, marginLeft: 32}}>Id: {venta.id}</Text>
+          <Text style={{marginTop: 16, marginLeft: 32}}>Nro Factura: {venta.prefijoFactura}{venta.nroFactura}</Text>
+          <Text style={{marginTop: 16, marginLeft: 32}}>Cliente: {venta.cliente.nombre}</Text>
+          <Text style={{marginTop: 16, marginLeft: 32}}>Fecha: {formatDate(venta.fecha)}</Text>
+          <Text style={{marginTop: 16, marginLeft: 32}}>Total: {venta.total}</Text>
+          <Text style={{marginTop: 32, marginLeft: 32, fontFamily: Fonts.BOLD, fontSize: 18}}>Detalles</Text>
           {venta.detalle.map((detalle, idx) => (<Detalle detalle={detalle} key={idx}/>))}
         </ScrollView>
       </View>
